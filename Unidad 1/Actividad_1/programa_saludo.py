49aa635ede2f6c47a78a0a0744167d680e57e5ca
alexisstdev <alexissanmiguel03@gmail.com>
2025-09-08 18:12:22 -0500

    Remove unnecessary comments

--- Unidad 1/Actividad_1/programa_saludo.py
+++ Unidad 1/Actividad_1/programa_saludo.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-"""
-Actividad 1 - Programación Multiparadigma 2025
-Primeros pasos con Python
-
-Autor: [Tu Nombre]
-Fecha: Septiembre 2025
-"""
-
 def main():
-    # Parte A.a - Mensaje de saludo simple
     print("Hola, este es mi primer programa en Python")
     
-    # Parte A.b - Saludo interactivo
     name = input("Ingresa tu nombre: ")
     age = input("Ingresa tu edad: ")
     
